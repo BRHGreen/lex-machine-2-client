89373aef9fff5bba29a8f09c160ee9ed8852781b
BRHGreen <benedictgreen@gmail.com>
2017-10-23 19:19:42 +0100

    saving token in local storage on login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,49 +1,103 @@
 import React from 'react';
-import { Button, Input } from 'antd'
-import { graphql } from 'react-apollo'
-import { loginMutation } from '../graphql/user'
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Message, Form, Button, Input, Container, Header } from 'semantic-ui-react';
+import { gql, graphql } from 'react-apollo';
 
 class Login extends React.Component {
-  state = {
-    email: '',
-    password: ''
-  }
+  constructor(props) {
+    super(props);
 
-  onChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      })
-    }
+    extendObservable(this, {
+      email: '',
+      password: '',
+      errors: {},
+    });
+  }
 
   onSubmit = async () => {
+    const { email, password } = this;
     const response = await this.props.mutate({
-      variables: this.state,
-    })
-    console.log('login res: ', response);
-  }
+      variables: { email, password },
+    });
 
-  render () {
-    console.log('props:', this.props);
+    console.log(response);
+
+    const {
+      ok, token, refreshToken, errors,
+    } = response.data.login;
+
+    if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+
+      this.errors = err;
+    }
+  };
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this[name] = value;
+  };
+
+  render() {
+    const { email, password, errors: { emailError, passwordError } } = this;
+
+    const errorList = [];
+
+    if (emailError) {
+      errorList.push(emailError);
+    }
+
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
+
     return (
-      <div>
-      <h1>Login</h1>
-        <Input
-          name='email'
-          placeholder='email'
-          onChange={e => this.onChange(e)}
-          value={this.state.email}
-          />
-        <Input
-          name='password'
-          placeholder='password'
-          type='password'
-          onChange={e => this.onChange(e)}
-          value={this.state.password}
-          />
-          <Button onClick={() => this.onSubmit()} type='primary'>Login</Button>
-      </div>
-    )
+      <Container text>
+        <Header as="h2">Login</Header>
+        <Form>
+          <Form.Field error={!!emailError}>
+            <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
+          </Form.Field>
+          <Form.Field error={!!passwordError}>
+            <Input
+              name="password"
+              onChange={this.onChange}
+              value={password}
+              type="password"
+              placeholder="Password"
+              fluid
+            />
+          </Form.Field>
+          <Button onClick={this.onSubmit}>Submit</Button>
+        </Form>
+        {errorList.length ? (
+          <Message error header="There was some errors with your submission" list={errorList} />
+        ) : null}
+      </Container>
+    );
   }
 }
 
-export default graphql(loginMutation)(Login)
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
