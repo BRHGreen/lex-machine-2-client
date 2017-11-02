c5e2e9963c425ade5f332316a9b10f05504db666
BRHGreen <benedictgreen@gmail.com>
2017-11-02 19:33:15 +0000

    authenticted routes working

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,52 @@
 import React from 'react'
+import decode from 'jwt-decode'
 import Home from './Home'
 import Register from './Register'
 import Login from './Login'
 import CreateWord from './CreateWord'
+
 import {
   BrowserRouter,
   Route,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom'
 
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken')
+  try {
+    decode(token);
+    decode(refreshToken);
+  } catch (err) {
+    return false
+  }
+  return true;
+}
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      (isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+      ))}
+  />
+);
 
 export default () =>(
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' render={props => <Home {...props} /> }/>
-      <Route exact path='/register' render={props => <Register {...props} /> }/>
-      <Route exact path='/login' render={props => <Login {...props} /> }/>
-      <Route exact path='/create-word' render={props => <CreateWord {...props} /> }/>
+      <Route path="/" exact component={Home} />
+      <Route path="/register" exact component={Register} />
+      <Route path="/login" exact component={Login} />
+      <PrivateRoute path="/create-word" exact component={CreateWord} />
     </Switch>
   </BrowserRouter>
 )
